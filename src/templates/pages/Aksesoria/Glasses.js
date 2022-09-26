a9af2a80c4ed42325c1342a53a6791550d20755d
ClonerG <clonergg@gmail.com>
2022-09-26 05:01:35 +0300

    1/2 navbar

--- src/templates/pages/Aksesoria/Glasses.js
+++ src/templates/pages/Aksesoria/Glasses.js
@@ -1,10 +1,27 @@
 import React from "react";
 import './style/glasses.css';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 
 const Glasses = () => {
   return (
-    <div class='glasses'>
-      <h2>Glasses</h2> 
+    <div class='one-chapter'>
+      <div class='sunglasses-shop'>
+        <div class='sunglasses-shop-s'></div>
+        <div class='sunglasses-shop-text'>SUNGLASSES SHOP</div>
+      </div>
+      <div class='navbar'>
+        <a class='navbar-text'>HOME</a>
+        <a class='navbar-text'>WOMEN&#39;S</a>
+        <a class='navbar-text'>MEN&#39;S</a>
+        <a class='navbar-text'>SALE</a>
+        <a class='navbar-text'>CONTACT</a>
+        <div class='navbar-login'>
+          <a><AccountCircleIcon class='navbar-icon'></AccountCircleIcon></a>
+          <a class='navbar-login-text'>LOG IN</a>
+          <a><ShoppingBagIcon class='navbar-icon'>0</ShoppingBagIcon></a>
+        </div>
+      </div>
     </div>
   );
 };
